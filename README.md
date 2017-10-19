6878758edd4d19b0bae9749402f5e206c3746c08
Sergey Khrustalev <sergey@khrustalev.net>
2017-10-19 18:06:29 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+This script loads information from devman.org api and prints table of users, who send their tasks for a verification after 24:00. 
+
+# Requirements
+
+This scripts require python3.5+
+See requirements.txt for a dependencies requirements.
+
+# How to run script on linux
+
+```bash
+$python seek_dev_nighters.py 
+Users who send their tasks for a verification after 24:00.
+First column is sent date, second is username
+19.10.2017 00.57.08      SergeiKhrustalev
+19.10.2017 00.41.22      kazimir_de_solar
+19.10.2017 00.06.17      sokolovav2016
+18.10.2017 00.35.22      SergeiKhrustalev
+18.10.2017 00.18.45      АлексейЦыкунов
+18.10.2017 00.09.31      SergeiKhrustalev
+17.10.2017 00.28.06      АртемЧерняев
+15.10.2017 00.56.42      artem7lomonosov
+15.10.2017 00.45.12      kazimir_de_solar
+15.10.2017 00.41.27      kazimir_de_solar
+15.10.2017 00.13.03      modumoduso
+15.10.2017 00.05.35      kazimir_de_solar
+15.10.2017 00.05.21      kazimir_de_solar
+15.10.2017 00.01.17      kazimir_de_solar
+14.10.2017 00.49.48      dm_shorokhov
+13.10.2017 00.53.01      АлексейЦыкунов
+```
+
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,46 @@
+from datetime import datetime
+import pytz
+import requests
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+
+    api_url = 'https://devman.org/api/challenges/solution_attempts/?page={}'
+    number_of_pages = requests.get(api_url.format(1)).json()['number_of_pages']
+
+    for page in range(number_of_pages):
+
+        current_page_records = requests.get(
+                api_url.format(page+1)
+            ).json()['records']
+
+        for attempts_record in current_page_records:
+            yield attempts_record
+
 
 def get_midnighters():
-    pass
+
+    server_timezone = pytz.timezone('Europe/Moscow')
+
+    for attempts_record in load_attempts():
+
+        server_datetime = server_timezone.localize(
+            datetime.fromtimestamp(attempts_record['timestamp']))
+        client_timezone = pytz.timezone(attempts_record['timezone'])
+        client_datetime = server_datetime.astimezone(client_timezone)
+
+        if client_datetime.hour == 0:
+            yield attempts_record
+
 
 if __name__ == '__main__':
-  pass
+
+    print('Users who send their tasks for a verification after 24:00.')
+    print('First column is sent datetime, second is username')
+
+    for attempts_records in get_midnighters():
+
+        client_datetime = datetime.fromtimestamp(
+            attempts_records['timestamp']).strftime('%d.%m.%Y %H.%M.%S')
+
+        print(client_datetime, '\t', attempts_records['username'])
